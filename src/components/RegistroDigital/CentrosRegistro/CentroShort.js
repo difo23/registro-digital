51fcab94e1ac0bdf66a7253716745f1fe1d96a93
difo23 <lizandro.ramirez@ucateci.edu.do>
2019-04-09 23:15:03 -0400

    Refactoring componente centro- Uso de layout

---
+++ src/components/RegistroDigital/CentrosRegistro/CentroShort.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+class CentroShort extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		return (
+			<div>
+				<h1>Hello David y Jerly</h1>
+			</div>
+		);
+	}
+}
+export default CentroShort;

0aaa8340f84962299757c793b49243961828b644
difo23 <lizandro.ramirez@ucateci.edu.do>
2019-04-14 21:24:31 -0400

    react-create-app on Heroku

---
+++ build/precache-manifest.1a5303c14b236f9dc2762e836e2a2d10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e288be290ffb66aee249",
+    "url": "/static/js/main.a35533ed.chunk.js"
+  },
+  {
+    "revision": "7d7fb7870464a2c89b79",
+    "url": "/static/js/2.7afac409.chunk.js"
+  },
+  {
+    "revision": "e288be290ffb66aee249",
+    "url": "/static/css/main.d948bf93.chunk.css"
+  },
+  {
+    "revision": "7d7fb7870464a2c89b79",
+    "url": "/static/css/2.18ca9390.chunk.css"
+  },
+  {
+    "revision": "48b3b7e88c3cf181b707de6b20c04fc7",
+    "url": "/index.html"
+  }
+];

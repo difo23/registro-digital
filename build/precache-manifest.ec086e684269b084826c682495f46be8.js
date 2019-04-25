bf870bfa1ec7c4a2cea0b252c8af24f5b6e569da
difo23 <lizandro.ramirez@ucateci.edu.do>
2019-04-24 21:48:21 -0400

    Build 2.0 integracion libro y centro

---
+++ build/precache-manifest.ec086e684269b084826c682495f46be8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "414608a417a171e0877551d4cc6b9022",
+    "url": "/static/media/logo.414608a4.jpeg"
+  },
+  {
+    "revision": "414608a417a171e0877551d4cc6b9022",
+    "url": "/static/media/cemas.414608a4.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "322128b7412d44816517",
+    "url": "/static/js/main.ea528e0f.chunk.js"
+  },
+  {
+    "revision": "14de495f76cce2db5b7e",
+    "url": "/static/js/2.52a73bf0.chunk.js"
+  },
+  {
+    "revision": "322128b7412d44816517",
+    "url": "/static/css/main.d948bf93.chunk.css"
+  },
+  {
+    "revision": "14de495f76cce2db5b7e",
+    "url": "/static/css/2.18ca9390.chunk.css"
+  },
+  {
+    "revision": "0723fb1a9b4262973590bc0b0c475f03",
+    "url": "/index.html"
+  }
+];
